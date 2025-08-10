da19dd7519babb83dcabf1de4c494d04a1a60ac7
TatsuyaOotani <ouroboros.public@gmail.com>
2025-08-10 12:46:15 +0900

    〇 Laravel11更新 〇 スライドショーの機能を追加

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Middleware\AuthPictConnectAccount;
 use App\Http\Middleware\CheckEventJoined;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,39 +23,40 @@ Route::middleware(AuthPictConnectAccount::class)->group(function () {
 
 // ajax routes
     Route::prefix('uploader')->name('uploader.')->group(function() {
-        Route::post('/photo-upload', 'Uploader\PhotoUploadController@photoStore')->name('photo_upload');
-        Route::post('/photo-delete', 'Uploader\PhotoDeleteController@photoDelete')->name('photo_delete');
+        Route::post('/photo-upload', [App\Http\Controllers\Uploader\PhotoUploadController::class, 'photoStore'])->name('photo_upload');
+        Route::post('/photo-delete', [App\Http\Controllers\Uploader\PhotoDeleteController::class, 'photoDelete'])->name('photo_delete');
     });
 
     Route::prefix('media')->name('media.')->group(function() {
         Route::prefix('/photo')->name('photo.')->group(function() {
             Route::middleware(CheckEventJoined::class)->group(function() {
-                Route::get('/list', 'MediaDistributor\GetPhotoController@getPhotosList')->name('list');
-//    Route::get('/prev-list', 'MediaDistributor\GetPhotoController@getPrevPhotosList')->name('photo_delete');
-                Route::get('/text-list', 'MediaDistributor\GetPhotoController@getTextPhotosList')->name('text_list');
+                Route::get('/list', [App\Http\Controllers\MediaDistributor\GetPhotoController::class, 'getPhotosList'])->name('list');
+//    Route::get('/prev-list', [App\Http\Controllers\MediaDistributor\GetPhotoController::class, 'getPrevPhotosList'])->name('photo_delete');
+                Route::get('/text-list', [App\Http\Controllers\MediaDistributor\GetPhotoController::class, 'getTextPhotosList'])->name('text_list');
             });
-                Route::get('/{photo_id}', 'MediaDistributor\GetPhotoController@getFullSizePhoto')->name('get_full_size_photo');
-                Route::get('/{photo_id}/thumbnail', 'MediaDistributor\GetPhotoController@getThumbnail')->name('thumbnail');
-                Route::get('/{photo_id}/download', 'MediaDistributor\GetPhotoController@downloadFullSizePhoto')->name('download');
+                Route::get('/{photo_id}', [App\Http\Controllers\MediaDistributor\GetPhotoController::class, 'getFullSizePhoto'])->name('get_full_size_photo');
+                Route::get('/{photo_id}/thumbnail', [App\Http\Controllers\MediaDistributor\GetPhotoController::class, 'getThumbnail'])->name('thumbnail');
+                Route::get('/{photo_id}/download', [App\Http\Controllers\MediaDistributor\GetPhotoController::class, 'downloadFullSizePhoto'])->name('download');
         });
-        Route::get('/profile-icon/{user_id}', 'MediaDistributor\GetPhotoController@getProfileIcon')->name('profile_icon');
-        Route::get('/event-icon/join-token/{joinToken}', 'MediaDistributor\GetEventIconController@getEventIconFromJoinToken')->name('event-icon.join-token');
+        Route::get('/profile-icon/{user_id}', [App\Http\Controllers\MediaDistributor\GetPhotoController::class, 'getProfileIcon'])->name('profile_icon');
+        Route::get('/event-icon/join-token/{joinToken}', [App\Http\Controllers\MediaDistributor\GetEventIconController::class, 'getEventIconFromJoinToken'])->name('event-icon.join-token');
         Route::middleware(CheckEventJoined::class)->group(function(){
-            Route::get('/event-icon/{eventId}', 'MediaDistributor\GetEventIconController@getEventIcon')->name('event-icon');
+            Route::get('/event-icon/{eventId}', [App\Http\Controllers\MediaDistributor\GetEventIconController::class, 'getEventIcon'])->name('event-icon');
         });
     });
 
     Route::prefix('event')->name('event.')->group(function() {
-        Route::get('/list/joined', 'EventsManage\EventsManageController@joinedList')->name('list.joined');
-        Route::get('/list/admin', 'EventsManage\EventsManageController@adminList')->name('list.admin');
-        Route::post('/create', 'EventsManage\EventsManageController@create')->name('create');
-        Route::get('/detail/join-token/{joinToken}', 'EventsManage\EventsManageController@getEventDetailFromToken')->name('detail.join_token');
+        Route::get('/list/joined', [App\Http\Controllers\EventsManage\EventsManageController::class, 'joinedList'])->name('list.joined');
+        Route::get('/list/admin', [App\Http\Controllers\EventsManage\EventsManageController::class, 'adminList'])->name('list.admin');
+        Route::post('/create', [App\Http\Controllers\EventsManage\EventsManageController::class, 'create'])->name('create');
+        Route::get('/detail/join-token/{joinToken}', [App\Http\Controllers\EventsManage\EventsManageController::class, 'getEventDetailFromToken'])->name('detail.join_token');
         Route::middleware(CheckEventJoined::class)->group(function(){
-            Route::get('/detail/{eventId}', 'EventsManage\EventsManageController@getEventDetail')->name('detail');
+            Route::get('/detail/{eventId}', [App\Http\Controllers\EventsManage\EventsManageController::class, 'getEventDetail'])->name('detail');
+            Route::get('/zip/{eventId}', [App\Http\Controllers\EventsManage\EventsManageController::class, 'getAllEventPhotos'])->name('get_all_event_photos');
         });
 
-        Route::post('/token/create/{eventId}', 'EventsManage\EventsManageController@createJoinToken')->name('token.create');
-        Route::post('/join/{joinToken}', 'EventsManage\EventsManageController@joinEvent')->name('join');
+        Route::post('/token/create/{eventId}', [App\Http\Controllers\EventsManage\EventsManageController::class, 'createJoinToken'])->name('token.create');
+        Route::post('/join/{joinToken}', [App\Http\Controllers\EventsManage\EventsManageController::class, 'joinEvent'])->name('join');
     });
 });
 
--- routes/web.php
+++ routes/web.php
@@ -25,11 +25,11 @@ Auth::routes();
 Route::view('welcome', 'welcome');
 
 // TwitterログインURL
-Route::get('login/twitter', 'Auth\TwitterController@redirectToProvider'); //標準ログインにそろえて
+Route::get('login/twitter', [App\Http\Controllers\Auth\TwitterController::class, 'redirectToProvider']); //標準ログインにそろえて
 // TwitterコールバックURL
-Route::get('auth/twitter/callback', 'Auth\TwitterController@handleProviderCallback');
+Route::get('auth/twitter/callback', [App\Http\Controllers\Auth\TwitterController::class, 'handleProviderCallback']);
 // TwitterログアウトURL
-Route::get('auth/twitter/logout', 'Auth\TwitterController@logout');
+Route::get('auth/twitter/logout', [App\Http\Controllers\Auth\TwitterController::class, 'logout']);
 
 Route::middleware(['cors'])->group(function () {
 
@@ -41,17 +41,17 @@ Route::middleware(['cors'])->group(function () {
     // 会員専用ページ
     Route::group(['middleware' => 'auth'], function () {
         // web ui routes
-//        Route::get('/', 'Viewer\EventLibController@index');
-        Route::get('/event/join/{joinToken}', 'Viewer\EventLibController@joinEvent');
-        Route::get('/event/joined', 'Viewer\EventLibController@index');
-        Route::middleware(CheckEventJoined::class)->get('/event/joined/{eventId}', 'Viewer\EventLibController@joindEvent');
-        Route::get('/event-grid-show', 'Viewer\EventLibController@index');
-        Route::view('/event-slide-show', 'viewer.pages.event-lib-slideshow');
+//        Route::get('/', [App\Http\Controllers\Viewer\EventLibController::class, 'index']);
+        Route::get('/event/join/{joinToken}', [App\Http\Controllers\Viewer\EventLibController::class, 'joinEvent']);
+        Route::get('/event/joined', [App\Http\Controllers\Viewer\EventLibController::class, 'index']);
+        Route::middleware(CheckEventJoined::class)->get('/event/joined/{eventId}', [App\Http\Controllers\Viewer\EventLibController::class, 'joindEvent']);
+        Route::get('/event-grid-show', [App\Http\Controllers\Viewer\EventLibController::class, 'index']);
+        Route::middleware(CheckEventJoined::class)->get('/event/joined/{eventId}/slide', [App\Http\Controllers\Viewer\EventLibController::class, 'slideShow']);
         Route::view('/users', 'viewer.pages.user-list');
         Route::view('/user-lib', 'viewer.pages.user-lib');
         Route::view('/event/create', 'viewer.pages.event-create');
-        Route::get('/user/edit', 'Viewer\UserEditController@show');
-        Route::post('/user/edit', 'Viewer\UserEditController@update');
+        Route::get('/user/edit', [App\Http\Controllers\Viewer\UserEditController::class, 'show']);
+        Route::post('/user/edit', [App\Http\Controllers\Viewer\UserEditController::class, 'update']);
     });
 
 });
